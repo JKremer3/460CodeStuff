fdd4ec85f38a1c0639b234826145c45d56ee9ed1
JKremer3 <jeffrey.kremer@wsu.edu>
2019-04-04 14:56:53 -0700

    Lab 5 stuff
    pre-lab folder contains actual full lab 5. zip folder of prelab has the actual prelab.

---
+++ lab5/prelab/USER/ucode.c
@@ -0,0 +1,153 @@
+typedef unsigned char   u8;
+typedef unsigned short  u16;
+typedef unsigned int    u32;
+
+#include "string.c"
+#include "uio.c"
+
+int ubody(char *name)
+{
+  int pid, ppid;
+  char line[64];
+  u32 mode,  *up;
+
+  mode = getcsr();
+  mode = mode & 0x1F;
+  printf("CPU mode=%x\n", mode);
+  pid = getpid();
+  ppid = getppid();
+
+  while(1){
+    printf("This is process #%d in Umode at %x parent=%d\n", pid, getPA(),ppid);
+    umenu();
+    printf("input a command : ");
+    ugetline(line); 
+    uprintf("\n"); 
+ 
+    if (strcmp(line, "getpid")==0)
+       ugetpid();
+    if (strcmp(line, "getppid")==0)
+       ugetppid();
+    if (strcmp(line, "ps")==0)
+       ups();
+    if (strcmp(line, "chname")==0)
+       uchname();
+    if(strcmp(line, "wait")==0)
+       ukwait();
+    if(strcmp(line, "sleep")==0)
+       uksleep();
+    if(strcmp(line, "wakeup")==0)
+       ukwakeup();
+    if(strcmp(line, "exit")==0)
+       ukbrexit();
+    if(strcmp(line, "fork")==0)
+       ukfork(name);
+    if (strcmp(line, "switch")==0)
+       uswitch();
+    if(strcmp(line, "exec")==0)
+       uexec();
+  }
+}
+
+int umenu()
+{
+  uprintf("-------------------------------------------------\n");
+  uprintf("getpid getppid ps chname switch sleep wakeup wait\n");
+  uprintf("exit fork exec\n");
+  uprintf("-------------------------------------------------\n");
+}
+
+int getpid()
+{
+  int pid;
+  pid = syscall(0,0,0,0);
+  return pid;
+}    
+
+int getppid()
+{ 
+  return syscall(1,0,0,0);
+}
+
+int ugetpid()
+{
+  int pid = getpid();
+  uprintf("pid = %d\n", pid);
+}
+
+int ugetppid()
+{
+  int ppid = getppid();
+  uprintf("ppid = %d\n", ppid);
+}
+
+int ups()
+{
+  return syscall(2,0,0,0);
+}
+
+int uchname()
+{
+  char s[32];
+  uprintf("input a name string : ");
+  ugetline(s);
+  printf("\n");
+  return syscall(3,s,0,0);
+}
+
+int uswitch()
+{
+  return syscall(4,0,0,0);
+}
+
+int uksleep()
+{
+  return syscall(5,0,0,0);
+}
+
+int ukwakeup()
+{
+  printf("Enter Sleep Code:  ");
+  int code= geti();
+  return syscall(6, code,0,0);
+  printf("\n");
+}
+
+int ukwait()
+{
+  return syscall(7,0,0,0);
+}
+
+int ukbrexit()
+{
+  return syscall(8,69,0,0);
+}
+
+int ukfork(char *name)
+{
+  return syscall(9,name,0,0);
+}
+
+
+int uexec()
+{
+  char s[32];
+  uprintf("input cmd: ");
+  ugetline(s);
+  return syscall(10, s, 0, 0);
+}
+
+int ugetc()
+{
+  return syscall(90,0,0,0);
+}
+
+int uputc(char c)
+{
+  return syscall(91,c,0,0);
+}
+
+int getPA()
+{
+  return syscall(92,0,0,0);
+}
---
+++ lab5/prelab/forkExec.c
@@ -0,0 +1,158 @@
+
+int body(), goUmode();
+
+PROC *kfork(char *filename)
+{
+  int i; 
+  int *ptable, pentry;
+  char *addr;
+  /*
+  char *cp, *cq;
+
+  char line[8];
+  int usize1, usize;
+  int *ustacktop, *usp;
+  u32 BA, Btop, Busp;
+  */
+  PROC *p = dequeue(&freeList);
+  if (p==0){
+    kprintf("kfork failed\n");
+    return (PROC *)0;
+  }
+  p->ppid = running->pid;
+  p->parent = running;
+  p->parent = running;
+  p->status = READY;
+  p->priority = 1;
+  p->cpsr = (int *)0x10;
+  p->sibling = 0;
+  p->child = 0;
+
+ // build p's pgtable 
+  p->pgdir = (int *)(0x600000 + (p->pid - 1)*0x4000);
+  ptable = p->pgdir;
+  // initialize pgtable
+  for (i=0; i<4096; i++)
+    ptable[i] = 0;
+  pentry = 0x412;
+  for (i=0; i<258; i++){
+    ptable[i] = pentry;
+    pentry += 0x100000;
+  }
+  // ptable entry flag=|AP0|doma|1|CB10|=110|0001|1|1110|=0xC3E or 0xC32       
+  //ptable[2048] = 0x800000 + (p->pid - 1)*0x100000|0xC3E;
+  ptable[2048] = 0x800000 + (p->pid - 1)*0x100000|0xC32;
+  
+  p->cpsr = (int *)0x10;    // previous mode was Umode
+  
+  if(p->parent->child == 0)
+  {
+    p->parent->child = p;
+  }
+  else
+  {
+    PROC *temp = p->parent->child;
+
+    while(temp->sibling != 0)
+    {
+      temp = temp->sibling;
+    }
+
+    temp->sibling = p;
+  }
+  // set kstack to resume to goUmode, then to VA=0 in Umode image
+  for (i=1; i<29; i++)  // all 28 cells = 0
+    p->kstack[SSIZE-i] = 0;
+
+  p->kstack[SSIZE-15] = (int)goUmode;  // in dec reg=address ORDER !!!
+  p->ksp = &(p->kstack[SSIZE-28]);
+
+  // kstack must contain a resume frame FOLLOWed by a goUmode frame
+  //  ksp  
+  //  -|-----------------------------------------
+  //  r0 r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 fp ip pc|
+  //  -------------------------------------------
+  //  28 27 26 25 24 23 22 21 20 19 18  17 16 15
+  //  
+  //   usp      NOTE: r0 is NOT saved in svc_entry()
+  // -|-----goUmode--------------------------------
+  //  r0 r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 ufp uip upc|
+  //-------------------------------------------------
+  //  14 13 12 11 10 9  8  7  6  5  4   3    2   1
+  /********************
+
+  // to go Umode, must set new PROC's Umode cpsr to Umode=10000
+  // this was done in ts.s dring init the mode stacks ==> 
+  // user mode's cspr was set to IF=00, mode=10000  
+
+  ***********************/
+  // must load filename to Umode image area at 7MB+(pid-1)*1MB
+  addr = (char *)(0x700000 + (p->pid)*0x100000);
+  kprintf("\n\npassing in %s\n\n", filename);
+  load(filename, p);
+  
+  // must fix Umode ustack for it to goUmode: how did the PROC come to Kmode?
+  // by swi # from VA=0 in Umode => at that time all CPU regs are 0
+  // we are in Kmode, p's ustack is at its Uimage (8mb+(pid-1)*1mb) high end
+  // from PROC's point of view, it's a VA at 1MB (from its VA=0)
+  // but we in Kmode must access p's Ustack directly
+
+  /***** this sets each proc's ustack differently, thinking each in 8MB+
+  ustacktop = (int *)(0x800000+(p->pid)*0x100000 + 0x100000);
+  TRY to set it to OFFSET 1MB in its section; regardless of pid
+  **********************************************************************/
+  //p->usp = (int *)(0x80100000);
+  p->usp = (int *)VA(0x100000);
+
+  //  p->kstack[SSIZE-1] = (int)0x80000000;
+  p->kstack[SSIZE-1] = VA(0);
+  // -|-----goUmode-------------------------------------------------
+  //  r0 r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 ufp uip upc|string       |
+  //----------------------------------------------------------------
+  //  14 13 12 11 10 9  8  7  6  5  4   3    2   1 |             |
+
+  enqueue(&readyQueue, p);
+
+  kprintf("proc %d kforked a child %d: ", running->pid, p->pid); 
+  printQ(readyQueue);
+
+  return p;
+}
+
+int kexec(char *cmdline)
+{
+    int i, upa, usp;
+    char *cp, kline[128], file[32], filename[32];
+    PROC *p = running;
+    kstrcpy(kline, cmdline);
+    //get first token of kline as filename
+    cp = kline; i = 0;
+    while(*cp != '\0'){
+        filename[i] = *cp;
+        i++; cp++;
+    }
+    filename[i] = 0;
+    file[0] = 0;
+    //if filename relative
+    // if(filename[0] != '/'){
+    //     kstrcpy(file, ""); //prefix with /bin/
+    // }
+    kstrcat(file, filename);
+    upa = p->pgdir[2048] & 0xFFFF0000; //PA of umode image
+    if(!load(file, p)){
+        return -1;
+    }
+
+    //copy cmdline to high end of Ustack in Umode image
+    usp = upa + 0x100000 - 128;
+    kstrcpy((char *) usp, kline);
+    p->usp = ((int *)VA(0x100000 - 128));
+    //fix syscall fram in kstack to return to VA=0 of new image
+    for(i = 2; i<14; i++){ //cleatr umode regs r1-r12
+        p->kstack[SSIZE - i] - 0;
+    }
+    // p->kstack[SSIZE-14] = (int)VA(p->usp);
+    p->kstack[SSIZE - 1] = (int)VA(0);
+
+    return (int)p->usp; //will replace saved r0 in the kstack
+}
